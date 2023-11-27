0e37afe63e22b67830cccf5a6c7bcb737c3f2581
lt <1355692441@qq.com>
2023-11-27 21:50:22 +0800

    测试

--- network_chat_room/generated/gui_guider.c
+++ network_chat_room/generated/gui_guider.c
@@ -27,6 +27,6 @@ void init_scr_del_flag(lv_ui *ui)
 void setup_ui(lv_ui *ui)
 {
 	init_scr_del_flag(ui);
-	setup_scr_screen_main(ui);
-	lv_scr_load(ui->screen_main);
+	setup_scr_screen_input(ui);
+	lv_scr_load(ui->screen_input);
 }
